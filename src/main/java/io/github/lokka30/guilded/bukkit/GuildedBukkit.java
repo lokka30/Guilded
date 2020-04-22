5de044159d9dfaa9d9251514f2e41fb1f8b5988f
lokka30 <lokka30@protonmail.com>
2020-04-22 08:58:11 +0800

    1.0.0-ALPHA

---
+++ src/main/java/io/github/lokka30/guilded/bukkit/GuildedBukkit.java
@@ -0,0 +1,11 @@
+package io.github.lokka30.guilded.bukkit;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class GuildedBukkit extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("It works!");
+    }
+}
---
+++ src/main/java/io/github/lokka30/guilded/bungee/GuildedBungee.java
@@ -0,0 +1,11 @@
+package io.github.lokka30.guilded.bungee;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class GuildedBungee extends Plugin {
+
+    @Override
+    public void onEnable() {
+        getLogger().info("It works!");
+    }
+}
